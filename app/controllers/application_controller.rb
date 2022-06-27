da5d5f3c6e759067efb7f2697fad0eaf16fb6a36
annomao <omaoannkerubo@gmail.com>
2022-06-27 12:10:00 +0300

    add CRUD report routes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,46 @@ class ApplicationController < Sinatra::Base
   
   # Add your routes here
   get "/" do
-    { message: "Good luck with your project!" }.to_json
+    "Welcome to iReport!." 
+  end
+
+  #reports routes
+  get "/reports/:id" do
+    reports = Report.all.where(["user_id= ?", params[:id]]).order(:updated_at)
+    reports.to_json
+  end
+
+  post "/reports" do
+      report = Report.create(
+      title:params[:title],
+      location:params[:location],
+      comment:params[:comment],
+      user_id:params[:user_id],
+      type_id:params[:type_id]
+    )
+    report.to_json
+  end
+
+  patch "/reports/location/:id" do
+    report = Report.find(params[:id])
+    report.update(
+      location:params[:comment]
+    )
+    report.to_json
+  end
+
+  patch "/reports/comment/:id" do
+    report = Report.find(params[:id])
+    report.update(
+      comment:params[:comment]
+    )
+    report.to_json
+  end
+
+  delete "/reports/:id" do
+    report = Report.find(params[:id])
+    report.destroy
+    report.to_json
   end
 
 end
